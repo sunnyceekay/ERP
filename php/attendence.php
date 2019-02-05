8177093d9871544577b91e3854e34f21b57d1e86
0001SID <mailofsumandas@gmail.com>
2019-02-05 20:01:06 +0530

    initiallization

---
+++ php/attendence.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Attencence Sheet</title>
+</head>
+<body>
+	<center><h1>Attendence Sheet</h1></center>
+
+	<?php
+
+		require "connection.php";
+		include "functions.php";
+		$query = "select name,roll_no from students;";
+		check_connection();
+
+	echo '<form>
+		Enter Date:
+		<input type = "date" />
+		<table>
+			<tr>
+				<th>Name</th>
+				<th>Roll No</th>
+				<th>Present</th>
+				<th>Absent</th>
+			</tr>';
+		
+		if($result = $conn->query($query))
+		{
+			while ($row = $result->fetch_assoc())
+			{
+
+				$name = $row['name'];
+				$roll = $row['roll_no'];
+				echo '
+				<tr>
+					<td>'.$name.'</td>
+					<td>'.$roll.'</td>
+					<td><input type = "radio" name ='."$roll".' value = "present" checked = "checked"/></td>
+					<td><input type = "radio" name ='."$roll".' value = "absent"/></td>
+				</tr>';
+
+			}
+		}
+		else{
+			echo 'Query failed'.$conn->error;
+		}
+		echo '</table></form>';
+	?>
+</body>
+</html>
